c9b0f19d85fc4f74fd681fb755b6b7d917e13408
victormartinezrubio@gmail.com <victormartinezrubio@gmail.com>
2023-02-10 16:31:20 +0100

    chore: Updated the content of the file "/var/folders/hn/j6gds1qx1f5by...
    ... sfn0qp0t03c0000gn/T/updatecli/github/v1v/e2e-testing/internal/common/defaults.go"
    
    Made with ❤️️ by updatecli

--- internal/common/defaults.go
+++ internal/common/defaults.go
@@ -36,7 +36,7 @@ const FleetProfileName = "fleet"
 const FleetServerAgentServiceName = "fleet-server"
 
 // BeatVersionBase is the base version of the Beat to use
-var BeatVersionBase = "8.7.0-1ef3b708-SNAPSHOT"
+var BeatVersionBase = = "8.6.2-7c4539d9-SNAPSHOT"
 
 // BeatVersion is the version of the Beat to use
 // It can be overriden by BEAT_VERSION env var
